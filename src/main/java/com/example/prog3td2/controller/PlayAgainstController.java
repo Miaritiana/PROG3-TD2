823fda43087c03dae881b808550706eb7e4504be
Miaritiana <hei.miaritiana@gmail.com>
2022-12-20 00:24:57 +0300

    feat: update all controller

--- src/main/java/com/example/prog3td2/controller/PlayAgainstController.java
+++ src/main/java/com/example/prog3td2/controller/PlayAgainstController.java
@@ -1,4 +1,51 @@
 package com.example.prog3td2.controller;
 
+import com.example.prog3td2.controller.mapper.playAgainstMapper;
+import com.example.prog3td2.controller.response.playagainstResponse.createPlayAgainstResponse;
+import com.example.prog3td2.controller.response.playagainstResponse.playAgainstResponse;
+import com.example.prog3td2.model.PlayAgainst;
+import com.example.prog3td2.service.PlayAgainstService;
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@AllArgsConstructor
 public class PlayAgainstController {
+    private final PlayAgainstService service;
+    private final playAgainstMapper mapper;
+
+    @GetMapping("/Match")
+    public List<playAgainstResponse> getPlayAgainst(){
+        return service.getPlayAgainst().stream()
+                .map(mapper::toRest)
+                .toList();
+    }
+    @PostMapping("/Match")
+    public List<playAgainstResponse> postPlayAgainst(@RequestBody List<createPlayAgainstResponse> playAgainsts){
+        List<PlayAgainst> playAgainst = playAgainsts.stream().map(mapper::toDomain).toList();
+        return service.postPlayAgainst(playAgainst).stream()
+                .map(mapper::toRest)
+                .toList();
+    }
+    @PutMapping("/Match")
+    public List<playAgainstResponse> putPlayAgainst(@RequestBody List<playAgainstResponse> playAgainsts){
+        List<PlayAgainst> toUpdate = playAgainsts.stream().map(mapper::toDomain).toList();
+        return service.putPlayAgainst(toUpdate).stream()
+                .map(mapper::toRest)
+                .toList();
+    }
+
+    @DeleteMapping("/Match/{id}")
+    public void deletePlayAgainst(@PathVariable int id){
+        service.deletePlayAgainst(id);
+    }
+
+    @GetMapping("/Match/{id}")
+    public Optional<PlayAgainst> findPlayAgainst(@PathVariable int id){
+        return service.findPlayAgainst(id);
+    }
 }
--- src/main/java/com/example/prog3td2/controller/PlayerController.java
+++ src/main/java/com/example/prog3td2/controller/PlayerController.java
@@ -1,4 +1,52 @@
 package com.example.prog3td2.controller;
 
+
+
+import com.example.prog3td2.controller.mapper.playerMapper;
+import com.example.prog3td2.controller.response.playerResponse.createPlayerResponse;
+import com.example.prog3td2.controller.response.playerResponse.playerResponse;
+import com.example.prog3td2.model.Player;
+import com.example.prog3td2.service.PlayerService;
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@AllArgsConstructor
 public class PlayerController {
+    private final PlayerService service;
+    private final playerMapper mapper;
+
+    @GetMapping("/Players")
+    public List<playerResponse> getPlayers(){
+        return service.getPlayers().stream()
+                .map(mapper::toRest)
+                .toList();
+    }
+    @PostMapping("/Players")
+    public List<playerResponse> postPlayers(@RequestBody List<createPlayerResponse> playerList){
+        List<Player> players = playerList.stream().map(mapper::toDomain).toList();
+        return service.postPlayers(players).stream()
+                .map(mapper::toRest)
+                .toList();
+    }
+    @PutMapping("/Players")
+    public List<playerResponse> putPlayers(@RequestBody List<playerResponse> players){
+        List<Player> toUpdate = players.stream().map(mapper::toDomain).toList();
+        return service.putPlayers(toUpdate).stream()
+                .map(mapper::toRest)
+                .toList();
+    }
+
+    @DeleteMapping("/Players/{id}")
+    public void deletePlayers(@PathVariable int id){
+        service.deletePlayers(id);
+    }
+
+    @GetMapping("/Players/{id}")
+    public Optional<Player> findPlayer(@PathVariable int id){
+        return service.findPlayers(id);
+    }
 }
